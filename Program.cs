4e580e6562c16036a43ec440d0e78cf17f0d61da
sui84 <sui84@126.com>
2017-06-19 13:43:03 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.IO;
 using System.Data;
 
-namespace ConvertHelper
+namespace ConvertPcap
 {
     public class Program
     {
@@ -16,7 +16,7 @@ namespace ConvertHelper
                 ConvertHelper cvt = new ConvertHelper();
                 string path = string.Empty;
                 if (args.Length == 0)
-                    path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"TXT/test170.txt");
+                    path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"TXT/https.txt");
                 else
                     path = args[0];
                 DataSet ds = cvt.OpenTXT(path, ':');
@@ -25,26 +25,30 @@ namespace ConvertHelper
                 string[] tbNames = new string[] { "ippkg", "otherpkg" };
                 for (int i = 0; i < ds.Tables.Count; i++)
                 {
-                    if (tbNames[i] == "ippkg")
+                    if (ds.Tables[i].Rows.Count > 0)
                     {
-                        mapCols[0] = cvt.GetIpPackageHead();
-                        mapCols[1] = new string[] { "FilePath","LineNumber","EncapsulationType", "ArrivalTime", "TimeShiftForThisPacket", "EpochTime", "TimeDeltaPrevCapFrame", "TimeDeltaPrevDisFrame", "TimeSinceReferOrFirstFrame"
-                            , "FrameNumber", "FrameLength", "CaptureLength", "FrameIsMarked", "FrameIsIgnored", "ProtocolsInFrame","Ethernet","Type","SenderMACAddress","SenderIPAddress","TargetMACAddress","TargetIPAddress","MS Network Load Balancing"
-                            , "Version", "HeaderLength", "DifferentiatedServicesField", "DifferentiatedServicesCodepoint", "ExplicitCongestionNotification", "TotalLength", "Identification"
-                            , "Flags", "Reservedbit", "NotFragment", "MoreFragments", "FragmentOffset", "TimeToLive", "Protocol", "HeaderChecksum", "HeaderChecksumStatus", "Source"
-                           , "SourceHost", "Destination", "DestinationHost", "SourceGeoIP", "DestinationGeoIP"
-                            , "SourcePort", "DestinationPort", "StreamIndex", "TCPSegmentLen", "SequenceNumber", "NextSequenceNumber", "AcknowledgmentNumber", "HeaderLength2"
-                            , "Flags2", "Reserved", "Nonce", "CongestionWindowReduced", "ECNEcho", "Urgent", "Acknowledgment", "Push", "Reset", "Syn", "Fin", "TCPFlags"
-                            , "WindowSizeValue", "Checksum", "UrgentPointer", "Options", "SEQACKAnalysis", "ACKFrame", "Timestamps", "TabularDataStream", "HypertextTransferProtocol", "BinaryData","BinaryDataStr"
-                           };
+                        if (tbNames[i] == "ippkg")
+                        {
+                            mapCols[0] = cvt.GetIpPackageHead();
+                            mapCols[1] = new string[] { "FilePath","LineNumber","EncapsulationType", "ArrivalTime", "TimeShiftForThisPacket", "EpochTime", "TimeDeltaPrevCapFrame", "TimeDeltaPrevDisFrame", "TimeSinceReferOrFirstFrame"
+                                , "FrameNumber", "FrameLength", "CaptureLength", "FrameIsMarked", "FrameIsIgnored", "ProtocolsInFrame","Ethernet","Type","SenderMACAddress","SenderIPAddress","TargetMACAddress","TargetIPAddress","MSNetworkLoadBalancing"
+                                , "Version", "HeaderLength", "DifferentiatedServicesField", "DifferentiatedServicesCodepoint", "ExplicitCongestionNotification", "TotalLength", "Identification"
+                                , "Flags", "Reservedbit", "NotFragment", "MoreFragments", "FragmentOffset", "TimeToLive", "Protocol", "HeaderChecksum", "HeaderChecksumStatus", "Source"
+                               , "SourceHost", "Destination", "DestinationHost", "SourceGeoIP", "DestinationGeoIP"
+                                , "SourcePort", "DestinationPort", "StreamIndex", "TCPSegmentLen", "SequenceNumber", "NextSequenceNumber", "AcknowledgmentNumber", "HeaderLength2"
+                               , "Flags2", "Reserved", "Nonce", "CongestionWindowReduced", "ECNEcho", "Urgent", "Acknowledgment", "Push", "Reset", "Syn", "Fin", "TCPFlags"
+                                , "WindowSizeValue", "Checksum", "UrgentPointer", "Options"
+                                , "SEQACKAnalysis", "ACKFrame", "Timestamps", "TabularDataStream", "HypertextTransferProtocol", "BinaryData","BinaryDataStr"
+                               };
+                        }
+                        else if (tbNames[i] == "otherpkg")
+                        {
+                            mapCols[0] = mapCols[1] = cvt.GetOtherPackageHead();
+                        }
+                         // dbhelper.ClearTable(tbNames[i], Properties.Settings.Default.ConnStr);
+                        dbhelper.DeleteTable(tbNames[i], string.Format(" where FilePath='{0}'", path), Properties.Settings.Default.ConnStr);
+                        dbhelper.CopyData(ds.Tables[i], tbNames[i], Properties.Settings.Default.ConnStr, mapCols);
                     }
-                    else if (tbNames[i] == "otherpkg")
-                    {
-                        mapCols[0] = mapCols[1] = cvt.GetOtherPackageHead();
-                    }
-                     // dbhelper.ClearTable(tbNames[i], Properties.Settings.Default.ConnStr);
-                    dbhelper.DeleteTable(tbNames[i], "where FilePath='{0}", Properties.Settings.Default.ConnStr);
-                    dbhelper.CopyData(ds.Tables[i], tbNames[i], Properties.Settings.Default.ConnStr, mapCols);
 
                 }
             }
@@ -63,3 +67,4 @@ namespace ConvertHelper
         }
     }
 }
+
