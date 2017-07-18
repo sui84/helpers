d7cf4fb28f915418462a1c1b6311b9fb90855742
sui84 <sui84@126.com>
2017-07-18 18:06:14 +0800

    Update TestSqlServerHelper.cs

--- VS/Common/Tests/TestSqlServerHelper.cs
+++ VS/Common/Tests/TestSqlServerHelper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +7,8 @@ using Microsoft.SqlServer.Management.Common;
 using Microsoft.SqlServer;
 using Microsoft.SqlServer.Management.Smo;
 using Microsoft.SqlServer.Management;
+using System.Data;
+using System.Threading;
 
 namespace Common.Tests
 {
@@ -37,37 +39,61 @@ namespace Common.Tests
     {
         static void Main(string[] args)
         {
+            try
+            {
 
-            SqlServerHelper sshelper = new SqlServerHelper();
-            
-            testtb t1 = new testtb();
-            t1.Int1 = int.MaxValue;
-            t1.Int2 = Int16.MaxValue;
-            t1.Int3 = Int32.MaxValue;
-            t1.Int4 = Int64.MaxValue;
-            t1.Byte1 = byte.MaxValue;
-            t1.Byte2 = Encoding.Default.GetBytes("test");
-            t1.Short1 = short.MaxValue;
-            t1.DateTime1 = DateTime.MinValue;
-            t1.Decimal1 = decimal.MaxValue;
-            t1.Bool1 = true;
-            t1.Bool2 = false;
-            t1.Float1 = float.MaxValue;
-            t1.Double1 = double.MaxValue;
-            t1.String1 = "你好ssadsa";
-            t1.Char1 ='9';
-            string servername = @"localhost\SQLEXPRESS";
-            string dbname = "newdb";
-            string username = "sa";
-            string password = "P@ssw0rd";
-            //ValueTypeHelper.IsNullable(t1.Int1);
-            t1.Int5 = 111;
-            ValueTypeHelper.IsNullable(t1.Int5);
-            DatabaseCollection dbs = sshelper.GetDatabases(servername);
-            Table tb = sshelper.CreateTable(servername,dbname, username, password, t1.GetType(),true);
-            tb.Script();
-            ConnStrHelper connhelper = new ConnStrHelper();
-            string connStr = connhelper.GetMSSQLClientConnStr(servername,dbname,username, password);
+                SqlServerHelper sshelper = new SqlServerHelper();
+
+                testtb t1 = new testtb();
+                t1.Int1 = int.MaxValue;
+                t1.Int2 = Int16.MaxValue;
+                t1.Int3 = Int32.MaxValue;
+                t1.Int4 = Int64.MaxValue;
+                t1.Byte1 = byte.MaxValue;
+                t1.Byte2 = Encoding.Default.GetBytes("test");
+                t1.Short1 = short.MaxValue;
+                t1.DateTime1 = DateTime.MaxValue;
+                t1.Decimal1 = decimal.MaxValue;
+                t1.Bool1 = true;
+                t1.Bool2 = false;
+                t1.Float1 = float.MaxValue;
+                t1.Double1 = double.MaxValue;
+                t1.String1 = "你好ssadsa";
+                t1.Char1 = '9';
+                string servername = @"localhost";
+                string dbname = "TEST";
+                string username = "sa";
+                string password = "password";
+                //ValueTypeHelper.IsNullable(t1.Int1);
+                t1.Int5 = 111;
+                ValueTypeHelper.IsNullable(t1.Int5);
+                DatabaseCollection dbs = sshelper.GetDatabases(servername);
+
+                Table tb = sshelper.CreateTable(servername, dbname, username, password, t1.GetType(), true);
+                tb.Script();
+                ConnStrHelper connhelper = new ConnStrHelper();
+                string connStr = connhelper.GetMSSQLClientConnStr(servername, dbname, username, password);
+
+                testtb t2 = new testtb();
+                t2.String1 = "你好";
+                t2.Byte2 = Encoding.Default.GetBytes("test2");
+                t2.DateTime1 = DateTime.MaxValue;
+                List<testtb> ts = new List<testtb>();
+                ts.Add(t1);
+                ts.Add(t2);
+                DataTable dt = DataTableHelper.CopyToDataTable(ts, null, null);
+                DbHelper dh = new DbHelper();
+                string[][] mapCols = new string[2][];
+                mapCols[0] = mapCols[1] = ReflectionHelper.GetObjectProperties(t1.GetType()).ToArray();
+                dh.CopyData(dt, t1.GetType().Name, connStr, mapCols);
+                //dh.CopyDataWithTran(dt,  t1.GetType().Name, connStr, null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Thread.Sleep(3000);
+            }
 
         }
     }
