e0c8d4adf28f827e6c9bd16471efd4d2b51638e9
sui84 <sui84@126.com>
2017-07-20 15:15:31 +0800

    Update CookieHelper.cs

--- VS/Common/Utils/CookieHelper.cs
+++ VS/Common/Utils/CookieHelper.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web;
 
 namespace Common.Utils
 {
@@ -32,5 +33,62 @@ namespace Common.Utils
         //In ASP.NET, when you modify web.config file, IIS will recycle the app pool. 	
         //HttpRuntime.UnloadAppDomain() 
         //File.SetLastWriteTime(this.Server.MapPath("~/web.config"), DateTime.Now);
+        #region Cookie操作
+        /// <summary>
+        /// 写cookie值
+        /// </summary>
+        /// <param name="strName">名称</param>
+        /// <param name="strValue">值</param>
+        public static void WriteCookie(string strName, string strValue)
+        {
+            HttpCookie cookie = HttpContext.Current.Request.Cookies[strName];
+            if (cookie == null)
+            {
+                cookie = new HttpCookie(strName);
+            }
+            cookie.Value = strValue;
+            HttpContext.Current.Response.AppendCookie(cookie);
+        }
+        /// <summary>
+        /// 写cookie值
+        /// </summary>
+        /// <param name="strName">名称</param>
+        /// <param name="strValue">值</param>
+        /// <param name="strValue">过期时间(分钟)</param>
+        public static void WriteCookie(string strName, string strValue, int expires)
+        {
+            HttpCookie cookie = HttpContext.Current.Request.Cookies[strName];
+            if (cookie == null)
+            {
+                cookie = new HttpCookie(strName);
+            }
+            cookie.Value = strValue;
+            cookie.Expires = DateTime.Now.AddMinutes(expires);
+            HttpContext.Current.Response.AppendCookie(cookie);
+        }
+        /// <summary>
+        /// 读cookie值
+        /// </summary>
+        /// <param name="strName">名称</param>
+        /// <returns>cookie值</returns>
+        public static string GetCookie(string strName)
+        {
+            if (HttpContext.Current.Request.Cookies != null && HttpContext.Current.Request.Cookies[strName] != null)
+            {
+                return HttpContext.Current.Request.Cookies[strName].Value.ToString();
+            }
+            return "";
+        }
+        /// <summary>
+        /// 删除Cookie对象
+        /// </summary>
+        /// <param name="CookiesName">Cookie对象名称</param>
+        public static void RemoveCookie(string CookiesName)
+        {
+            HttpCookie objCookie = new HttpCookie(CookiesName.Trim());
+            objCookie.Expires = DateTime.Now.AddYears(-5);
+            HttpContext.Current.Response.Cookies.Add(objCookie);
+        }
+        #endregion
     }
 }
