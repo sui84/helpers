24b65e0c20840604ad6aa85dbf0ac4972e03227d
sui84 <sui84@126.com>
2017-07-20 15:11:32 +0800

    Create WebRequestHelper.cs

---
+++ VS/Common/Utils/Web/WebRequestHelper.cs
@@ -0,0 +1,539 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Net;
+using System.IO;
+
+namespace Common.Utils.Web
+{
+    public class WebRequestHelper
+    {
+        public static string GetHostNameOrIP()
+        {
+            string hostname = null;
+            String clientIP = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (string.IsNullOrEmpty(clientIP))
+            {
+                clientIP = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+
+            }
+            try
+            {
+                IPHostEntry iPHostEntry = Dns.GetHostEntry(clientIP);
+                if (iPHostEntry != null)
+                {
+                    hostname = iPHostEntry.HostName;
+                }
+            }
+            catch { }
+
+            if (string.IsNullOrEmpty(hostname))
+                hostname = clientIP;
+
+            return hostname;
+        }
+
+        /// <summary>
+        /// 获取浏览器信息
+        /// </summary>
+        public static string GetBrowser()
+        { 
+            if (HttpContext.Current == null)
+                return string.Empty;
+            var browser = HttpContext.Current.Request.Browser;
+            return string.Format("{0} {1}", browser.Browser, browser.Version);
+       
+        }
+
+        #region ResolveUrl(解析相对Url)
+        /// <summary>
+        /// 解析相对Url
+        /// </summary>
+        /// <param name="relativeUrl">相对Url</param>
+        public static string ResolveUrl(string relativeUrl)
+        {
+            if (string.IsNullOrWhiteSpace(relativeUrl))
+                return string.Empty;
+            relativeUrl = relativeUrl.Replace("\\", "/");
+            if (relativeUrl.StartsWith("/"))
+                return relativeUrl;
+            if (relativeUrl.Contains("://"))
+                return relativeUrl;
+            return VirtualPathUtility.ToAbsolute(relativeUrl);
+        }
+
+        #endregion
+
+        #region HtmlEncode(对html字符串进行编码)
+        /// <summary>
+        /// 对html字符串进行编码
+        /// </summary>
+        /// <param name="html">html字符串</param>
+        public static string HtmlEncode(string html)
+        {
+            return HttpUtility.HtmlEncode(html);
+        }
+        /// <summary>
+        /// 对html字符串进行解码
+        /// </summary>
+        /// <param name="html">html字符串</param>
+        public static string HtmlDecode(string html)
+        {
+            return HttpUtility.HtmlDecode(html);
+        }
+
+        #endregion
+
+        #region UrlEncode(对Url进行编码)
+
+        /// <summary>
+        /// 对Url进行编码
+        /// </summary>
+        /// <param name="url">url</param>
+        /// <param name="isUpper">编码字符是否转成大写,范例,"http://"转成"http%3A%2F%2F"</param>
+        public static string UrlEncode(string url, bool isUpper = false)
+        {
+            return UrlEncode(url, Encoding.UTF8, isUpper);
+        }
+
+        /// <summary>
+        /// 对Url进行编码
+        /// </summary>
+        /// <param name="url">url</param>
+        /// <param name="encoding">字符编码</param>
+        /// <param name="isUpper">编码字符是否转成大写,范例,"http://"转成"http%3A%2F%2F"</param>
+        public static string UrlEncode(string url, Encoding encoding, bool isUpper = false)
+        {
+            var result = HttpUtility.UrlEncode(url, encoding);
+            if (!isUpper)
+                return result;
+            return GetUpperEncode(result);
+        }
+
+        /// <summary>
+        /// 获取大写编码字符串
+        /// </summary>
+        private static string GetUpperEncode(string encode)
+        {
+            var result = new StringBuilder();
+            int index = int.MinValue;
+            for (int i = 0; i < encode.Length; i++)
+            {
+                string character = encode[i].ToString();
+                if (character == "%")
+                    index = i;
+                if (i - index == 1 || i - index == 2)
+                    character = character.ToUpper();
+                result.Append(character);
+            }
+            return result.ToString();
+        }
+
+        #endregion
+        #region UrlDecode(对Url进行解码)
+
+        /// <summary>
+        /// 对Url进行解码,对于javascript的encodeURIComponent函数编码参数,应使用utf-8字符编码来解码
+        /// </summary>
+        /// <param name="url">url</param>
+        public static string UrlDecode(string url)
+        {
+            return HttpUtility.UrlDecode(url);
+        }
+
+        /// <summary>
+        /// 对Url进行解码,对于javascript的encodeURIComponent函数编码参数,应使用utf-8字符编码来解码
+        /// </summary>
+        /// <param name="url">url</param>
+        /// <param name="encoding">字符编码,对于javascript的encodeURIComponent函数编码参数,应使用utf-8字符编码来解码</param>
+        public static string UrlDecode(string url, Encoding encoding)
+        {
+            return HttpUtility.UrlDecode(url, encoding);
+        }
+
+        #endregion
+        #region Session操作
+        /// <summary>
+        /// 写Session
+        /// </summary>
+        /// <typeparam name="T">Session键值的类型</typeparam>
+        /// <param name="key">Session的键名</param>
+        /// <param name="value">Session的键值</param>
+        public static void WriteSession<T>(string key, T value)
+        {
+            if (string.IsNullOrEmpty(key))
+                return;
+            HttpContext.Current.Session[key] = value;
+        }
+
+        /// <summary>
+        /// 写Session
+        /// </summary>
+        /// <param name="key">Session的键名</param>
+        /// <param name="value">Session的键值</param>
+        public static void WriteSession(string key, string value)
+        {
+            WriteSession<string>(key, value);
+        }
+
+        /// <summary>
+        /// 读取Session的值
+        /// </summary>
+        /// <param name="key">Session的键名</param>        
+        public static string GetSession(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                return string.Empty;
+            return HttpContext.Current.Session[key] as string;
+        }
+        /// <summary>
+        /// 删除指定Session
+        /// </summary>
+        /// <param name="key">Session的键名</param>
+        public static void RemoveSession(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                return;
+            HttpContext.Current.Session.Contents.Remove(key);
+        }
+
+        #endregion
+
+
+
+        #region HttpWebRequest(请求网络资源)
+
+        /// <summary>
+        /// 请求网络资源,返回响应的文本
+        /// </summary>
+        /// <param name="url">网络资源地址</param>
+        public static string HttpWebRequest(string url)
+        {
+            return HttpWebRequest(url, string.Empty, Encoding.GetEncoding("utf-8"));
+        }
+
+        /// <summary>
+        /// 请求网络资源,返回响应的文本
+        /// </summary>
+        /// <param name="url">网络资源Url地址</param>
+        /// <param name="parameters">提交的参数,格式：参数1=参数值1&amp;参数2=参数值2</param>
+        public static string HttpWebRequest(string url, string parameters)
+        {
+            return HttpWebRequest(url, parameters, Encoding.GetEncoding("utf-8"), true);
+        }
+
+        /// <summary>
+        /// 请求网络资源,返回响应的文本
+        /// </summary>
+        /// <param name="url">网络资源地址</param>
+        /// <param name="parameters">提交的参数,格式：参数1=参数值1&amp;参数2=参数值2</param>
+        /// <param name="encoding">字符编码</param>
+        /// <param name="isPost">是否Post提交</param>
+        /// <param name="contentType">内容类型</param>
+        /// <param name="cookie">Cookie容器</param>
+        /// <param name="timeout">超时时间</param>
+        public static string HttpWebRequest(string url, string parameters, Encoding encoding, bool isPost = false,
+             string contentType = "application/x-www-form-urlencoded", CookieContainer cookie = null, int timeout = 120000)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Timeout = timeout;
+            request.CookieContainer = cookie;
+            if (isPost)
+            {
+                byte[] postData = encoding.GetBytes(parameters);
+                request.Method = "POST";
+                request.ContentType = contentType;
+                request.ContentLength = postData.Length;
+                using (Stream stream = request.GetRequestStream())
+                {
+                    stream.Write(postData, 0, postData.Length);
+                }
+            }
+            var response = (HttpWebResponse)request.GetResponse();
+            string result;
+            using (Stream stream = response.GetResponseStream())
+            {
+                if (stream == null)
+                    return string.Empty;
+                using (var reader = new StreamReader(stream, encoding))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+            return result;
+        }
+
+        #endregion
+
+        #region URL请求数据
+        /// <summary>
+        /// 执行URL获取页面内容
+        /// </summary>
+        public static string UrlExecute(string urlPath)
+        {
+            if (string.IsNullOrEmpty(urlPath))
+            {
+                return "error";
+            }
+            StringWriter sw = new StringWriter();
+            try
+            {
+                HttpContext.Current.Server.Execute(urlPath, sw);
+                return sw.ToString();
+            }
+            catch (Exception)
+            {
+                return "error";
+            }
+            finally
+            {
+                sw.Close();
+                sw.Dispose();
+            }
+        }
+        #endregion
+
+        #region "获取页面url"
+        /// <summary>
+        /// 获取当前访问页面地址参数
+        /// </summary>
+        public static string GetScriptNameQueryString
+        {
+            get
+            {
+                return HttpContext.Current.Request.ServerVariables["QUERY_STRING"].ToString();
+            }
+        }
+        /// <summary>
+        /// 获取当前访问页面地址
+        /// </summary>
+        public static string GetScriptName
+        {
+            get
+            {
+                return HttpContext.Current.Request.ServerVariables["SCRIPT_NAME"].ToString();
+            }
+        }
+        /// <summary>
+        /// 获取当前访问页面Url
+        /// </summary>
+        public static string GetScriptUrl
+        {
+            get
+            {
+                return GetScriptNameQueryString == "" ? GetScriptName : string.Format("{0}?{1}", GetScriptName, GetScriptNameQueryString);
+            }
+        }
+        /// <summary>
+        ///  获取当前访问页面 参数
+        /// </summary>
+        public static string GetScriptNameQuery
+        {
+            get
+            {
+                return HttpContext.Current.Request.Url.Query;
+            }
+        }
+        #endregion
+
+        #region URL处理
+        /// <summary>
+        /// URL字符编码
+        /// </summary>
+        public static string UrlEncode(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return "";
+            }
+            str = str.Replace("'", "");
+            return HttpContext.Current.Server.UrlEncode(str);
+        }
+
+        #endregion
+
+        #region URL中去除指定参数
+        /// <summary>
+        /// 中去除指定参数
+        /// </summary>
+        /// <param name="url">地址</param>
+        /// <param name="param">参数</param>
+        /// <returns></returns>
+        public static string buildurl(string url, string param)
+        {
+            string url1 = url;
+            if (url.IndexOf(param) > 0)
+            {
+                if (url.IndexOf("&", url.IndexOf(param) + param.Length) > 0)
+                {
+                    url1 = url.Substring(0, url.IndexOf(param) - 1) + url.Substring(url.IndexOf("&", url.IndexOf(param) + param.Length) + 1);
+                }
+                else
+                {
+                    url1 = url.Substring(0, url.IndexOf(param) - 1);
+                }
+                return url1;
+            }
+            else
+            {
+                return url1;
+            }
+        }
+        #endregion
+
+
+        /// <summary>
+        /// 判断当前页面是否接收到了Post请求
+        /// </summary>
+        /// <returns>是否接收到了Post请求</returns>
+        public static bool IsPost()
+        {
+            return HttpContext.Current.Request.HttpMethod.Equals("POST");
+        }
+
+        /// <summary>
+        /// 判断当前页面是否接收到了Get请求
+        /// </summary>
+        /// <returns>是否接收到了Get请求</returns>
+        public static bool IsGet()
+        {
+            return HttpContext.Current.Request.HttpMethod.Equals("GET");
+        }
+
+        /// <summary>
+        /// 返回指定的服务器变量信息
+        /// </summary>
+        /// <param name="strName">服务器变量名</param>
+        /// <returns>服务器变量信息</returns>
+        public static string GetServerString(string strName)
+        {
+            if (HttpContext.Current.Request.ServerVariables[strName] == null)
+                return "";
+
+            return HttpContext.Current.Request.ServerVariables[strName].ToString();
+        }
+
+        /// <summary>
+        /// 返回上一个页面的地址
+        /// </summary>
+        /// <returns>上一个页面的地址</returns>
+        public static string GetUrlReferrer()
+        {
+            string retVal = null;
+
+            try
+            {
+                retVal = HttpContext.Current.Request.UrlReferrer.ToString();
+            }
+            catch { }
+
+            if (retVal == null)
+                return "";
+
+            return retVal;
+        }
+
+        /// <summary>
+        /// 得到当前完整主机头
+        /// </summary>
+        /// <returns></returns>
+        public static string GetCurrentFullHost()
+        {
+            HttpRequest request = System.Web.HttpContext.Current.Request;
+            if (!request.Url.IsDefaultPort)
+                return string.Format("{0}:{1}", request.Url.Host, request.Url.Port.ToString());
+
+            return request.Url.Host;
+        }
+
+        /// <summary>
+        /// 得到主机头
+        /// </summary>
+        public static string GetHost()
+        {
+            return HttpContext.Current.Request.Url.Host;
+        }
+
+        /// <summary>
+        /// 得到主机名
+        /// </summary>
+        public static string GetDnsSafeHost()
+        {
+            return HttpContext.Current.Request.Url.DnsSafeHost;
+        }
+
+        /// <summary>
+        /// 获取当前请求的原始 URL(URL 中域信息之后的部分,包括查询字符串(如果存在))
+        /// </summary>
+        /// <returns>原始 URL</returns>
+        public static string GetRawUrl()
+        {
+            return HttpContext.Current.Request.RawUrl;
+        }
+
+        /// <summary>
+        /// 判断当前访问是否来自浏览器软件
+        /// </summary>
+        /// <returns>当前访问是否来自浏览器软件</returns>
+        public static bool IsBrowserGet()
+        {
+            string[] BrowserName = { "ie", "opera", "netscape", "mozilla", "konqueror", "firefox" };
+            string curBrowser = HttpContext.Current.Request.Browser.Type.ToLower();
+            for (int i = 0; i < BrowserName.Length; i++)
+            {
+                if (curBrowser.IndexOf(BrowserName[i]) >= 0)
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 判断是否来自搜索引擎链接
+        /// </summary>
+        /// <returns>是否来自搜索引擎链接</returns>
+        public static bool IsSearchEnginesGet()
+        {
+            if (HttpContext.Current.Request.UrlReferrer == null)
+                return false;
+
+            string[] SearchEngine = { "google", "yahoo", "msn", "baidu", "sogou", "sohu", "sina", "163", "lycos", "tom", "yisou", "iask", "soso", "gougou", "zhongsou" };
+            string tmpReferrer = HttpContext.Current.Request.UrlReferrer.ToString().ToLower();
+            for (int i = 0; i < SearchEngine.Length; i++)
+            {
+                if (tmpReferrer.IndexOf(SearchEngine[i]) >= 0)
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 获得当前完整Url地址
+        /// </summary>
+        /// <returns>当前完整Url地址</returns>
+        public static string GetUrl()
+        {
+            return HttpContext.Current.Request.Url.ToString();
+        }
+
+        /// <summary>
+        /// 获得当前页面的名称
+        /// </summary>
+        /// <returns>当前页面的名称</returns>
+        public static string GetPageName()
+        {
+            string[] urlArr = HttpContext.Current.Request.Url.AbsolutePath.Split('/');
+            return urlArr[urlArr.Length - 1].ToLower();
+        }
+
+        /// <summary>
+        /// 返回表单或Url参数的总个数
+        /// </summary>
+        /// <returns></returns>
+        public static int GetParamCount()
+        {
+            return HttpContext.Current.Request.Form.Count + HttpContext.Current.Request.QueryString.Count;
+        }
+    }
+}
