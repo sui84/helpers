5a22522864b0166221b1be0ab725a890a1eac30f
sui84 <sui84@126.com>
2017-07-20 15:05:38 +0800

    Create NPOIExcelHelper.cs

---
+++ VS/Common/Utils/File/NPOIExcelHelper.cs
@@ -0,0 +1,72 @@
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using System.Data;
+using System.IO;
+
+namespace Common.Utils.Files
+{
+    public class NPOIExcelHelper
+    {
+        private string _title;
+        private string _sheetName;
+
+        /// <summary>
+        /// 导出到Excel
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public bool ToExcel(DataTable table, string filePath)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            IWorkbook workBook = new HSSFWorkbook();
+            this._sheetName = string.IsNullOrEmpty(this._sheetName) ? "sheet1" : this._sheetName;
+            ISheet sheet = workBook.CreateSheet(this._sheetName);
+
+            //处理表格标题
+            IRow row = sheet.CreateRow(0);
+            row.CreateCell(0).SetCellValue(this._title);
+            sheet.AddMergedRegion(new CellRangeAddress(0, 0, 0, table.Columns.Count - 1));
+            row.Height = 500;
+
+            ICellStyle cellStyle = workBook.CreateCellStyle();
+            IFont font = workBook.CreateFont();
+            font.FontName = "微软雅黑";
+            font.FontHeightInPoints = 17;
+            cellStyle.SetFont(font);
+            cellStyle.VerticalAlignment = VerticalAlignment.Center;
+            cellStyle.Alignment = HorizontalAlignment.Center;
+            row.Cells[0].CellStyle = cellStyle;
+
+            //处理表格列头
+            row = sheet.CreateRow(1);
+            for (int i = 0; i < table.Columns.Count; i++)
+            {
+                row.CreateCell(i).SetCellValue(table.Columns[i].ColumnName);
+                row.Height = 350;
+                sheet.AutoSizeColumn(i);
+            }
+
+            //处理数据内容
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                row = sheet.CreateRow(2 + i);
+                row.Height = 250;
+                for (int j = 0; j < table.Columns.Count; j++)
+                {
+                    row.CreateCell(j).SetCellValue(table.Rows[i][j].ToString());
+                    sheet.SetColumnWidth(j, 256 * 15);
+                }
+            }
+
+            //写入数据流
+            workBook.Write(fs);
+            fs.Flush();
+            fs.Close();
+
+            return true;
+        }
+
+ 
+    }
+}
