f838c03209fc636fc80459e94c3da614fe43f082
sui84 <sui84@126.com>
2017-11-30 14:50:24 +0800

    Update WebRequestHelper.cs

--- VS/Common/Utils/Web/WebRequestHelper.cs
+++ VS/Common/Utils/Web/WebRequestHelper.cs
@@ -5,11 +5,153 @@ using System.Text;
 using System.Web;
 using System.Net;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 namespace Common.Utils.Web
 {
     public class WebRequestHelper
     {
+private  readonly string DefaultUserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)";
+        /// <summary>  
+        /// 创建GET方式的HTTP请求  
+        /// </summary>  
+        /// <param name="url">请求的URL</param>  
+        /// <param name="timeout">请求的超时时间</param>  
+        /// <param name="userAgent">请求的客户端浏览器信息，可以为空</param>  
+        /// <param name="cookies">随同HTTP请求发送的Cookie信息，如果不需要身份验证可以为空</param>  
+        /// <returns></returns>  
+        public  HttpWebResponse CreateGetHttpResponse(string url, int? timeout, string userAgent, CookieCollection cookies)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentNullException("url");
+            }
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.Method = "GET";
+            request.UserAgent = DefaultUserAgent;
+            if (!string.IsNullOrEmpty(userAgent))
+            {
+                request.UserAgent = userAgent;
+            }
+            if (timeout.HasValue)
+            {
+                request.Timeout = timeout.Value;
+            }
+            if (cookies != null)
+            {
+                request.CookieContainer = new CookieContainer();
+                request.CookieContainer.Add(cookies);
+            }
+            return request.GetResponse() as HttpWebResponse;
+        }
+        /// <summary>  
+        /// 创建POST方式的HTTP请求  
+        /// </summary>  
+        /// <param name="url">请求的URL</param>  
+        /// <param name="parameters">随同请求POST的参数名称及参数值字典</param>  
+        /// <param name="timeout">请求的超时时间</param>  
+        /// <param name="userAgent">请求的客户端浏览器信息，可以为空</param>  
+        /// <param name="requestEncoding">发送HTTP请求时所用的编码</param>  
+        /// <param name="cookies">随同HTTP请求发送的Cookie信息，如果不需要身份验证可以为空</param>  
+        /// <returns></returns>  
+        public  HttpWebResponse CreatePostHttpResponse(string url, IDictionary<string, string> parameters, int? timeout, string userAgent, Encoding requestEncoding, CookieCollection cookies)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentNullException("url");
+            }
+            if (requestEncoding == null)
+            {
+                throw new ArgumentNullException("requestEncoding");
+            }
+            HttpWebRequest request = null;
+            //如果是发送HTTPS请求  
+            if (url.StartsWith("https", StringComparison.OrdinalIgnoreCase))
+            {
+                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+                request = WebRequest.Create(url) as HttpWebRequest;
+                request.ProtocolVersion = HttpVersion.Version10;
+            }
+            else
+            {
+                request = WebRequest.Create(url) as HttpWebRequest;
+            }
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.Headers.Add("Accept-Encoding","gzip,deflate");
+
+
+            if (!string.IsNullOrEmpty(userAgent))
+            {
+                request.UserAgent = userAgent;
+            }
+            else
+            {
+                request.UserAgent = DefaultUserAgent;
+            }
+
+            if (timeout.HasValue)
+            {
+                request.Timeout = timeout.Value;
+            }
+            if (cookies != null)
+            {
+                request.CookieContainer = new CookieContainer();
+                request.CookieContainer.Add(cookies);
+            }
+            //如果需要POST数据  
+            if (!(parameters == null || parameters.Count == 0))
+            {
+                StringBuilder buffer = new StringBuilder();
+                int i = 0;
+                foreach (string key in parameters.Keys)
+                {
+                    if (i > 0)
+                    {
+                        buffer.AppendFormat("&{0}={1}", key, parameters[key]);
+                    }
+                    else
+                    {
+                        buffer.AppendFormat("{0}={1}", key, parameters[key]);
+                    }
+                    i++;
+                }
+                byte[] data = requestEncoding.GetBytes(buffer.ToString());
+                using (Stream stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            return request.GetResponse() as HttpWebResponse;
+        }
+
+        private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        {
+            return true; //总是接受  
+        }  
+
+        //StringBuilder sb = new StringBuilder();
+        //sb.AppendLine(string.Format("请求开始时间：{0}<br/>",DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")));
+        //sb.AppendLine(string.Format("服务器名称：{0}<br/>", Server.MachineName));
+        //sb.AppendLine(string.Format("服务器IP地址：{0}<br/>", Request.ServerVariables["LOCAL_ADDR"]));
+        //sb.AppendLine(string.Format("HTTP访问端口：{0}<br/>", Request.ServerVariables["SERVER_PORT"]));
+        //sb.AppendLine(string.Format(".NET解释引擎版本：.NET CLR{0}<br/>", Environment.Version.Major + "." + Environment.Version.Minor + "." + Environment.Version.Build + "." + Environment.Version.Revision));
+        //sb.AppendLine(string.Format("服务器操作系统版本：{0}<br/>", Environment.OSVersion.ToString()));
+        //sb.AppendLine(string.Format("服务器IIS版本：{0}<br/>", Request.ServerVariables["SERVER_SOFTWARE"]));
+        //sb.AppendLine(string.Format("服务器域名：{0}<br/>", Request.ServerVariables["SERVER_NAME"]));
+        //sb.AppendLine(string.Format("虚拟目录的绝对路径：{0}<br/>", Request.ServerVariables["APPL_RHYSICAL_PATH"]));
+        //sb.AppendLine(string.Format("执行文件的绝对路径：{0}<br/>", Request.ServerVariables["PATH_TRANSLATED"]));
+        //sb.AppendLine(string.Format("虚拟目录RedisSession总数：{0}<br/>", RedisSession.Count));
+        //sb.AppendLine(string.Format("虚拟目录Application总数：{0}<br/>", Application.Contents.Count.ToString()));
+        //sb.AppendLine(string.Format("域名主机：{0}<br/>", Request.ServerVariables["HTTP_HOST"]));
+        //sb.AppendLine(string.Format("服务器区域语言：{0}<br/>", Request.ServerVariables["HTTP_ACCEPT_LANGUAGE"]));
+        //sb.AppendLine(string.Format("用户信息：{0}<br/>", Request.ServerVariables["HTTP_USER_AGENT"]));
+        //sb.AppendLine(string.Format("CPU个数：{0}<br/>", Environment.GetEnvironmentVariable("NUMBER_OF_PROCESSORS")));
+        //sb.AppendLine(string.Format("CPU类型：{0}<br/>", Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER")));
+        //sb.AppendLine(string.Format("请求来源地址：{0}<br/>", Request.Headers["X-Real-IP"]));
+
+        
         public static string GetHostNameOrIP()
         {
             string hostname = null;
